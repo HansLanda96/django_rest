02909fa894d8d6bd45bea60729e3c873b3a06d24
Iegor Marchenko <duuurrr17@gmail.com>
2022-12-29 21:57:32 +0200

    initial commit

---
+++ shelf/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Author
+
+
+class AuthorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100, required=True)
+    country = serializers.CharField(
+        max_length=100, required=False, allow_blank=True, allow_null=True
+    )
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.country = validated_data.get("country", instance.country)
+        instance.save()
+        return instance
+
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
